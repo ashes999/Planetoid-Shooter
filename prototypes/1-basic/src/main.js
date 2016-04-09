85b002baad47f5ec1f406a19d2ca0b13a61de355
Ashiq A <ashes999@yahoo.com>
2016-04-09 14:26:04 -0400

    Repeating background! Scrolling world!

--- prototypes/1-basic/src/main.js
+++ prototypes/1-basic/src/main.js
@@ -1,25 +1,33 @@
 Game = {
     world: {
-        width: 800,
-        height: 600
+        width: 1600,
+        height: 1200
     },
-    
+
     // This defines our grid's size and the size of each of its tiles
     view: {
-        width: 400,
-        height: 300,
+        width: 800,
+        height: 600,
     },
 
     start: function() {
-        Crafty.init(Game.world.width, Game.world.height);
+        Crafty.init(Game.view.width, Game.view.height);
         loadImages(["images/background.jpg"], function() {
-            Crafty.e('Actor').img('images/background.jpg').move(0, 0).size(Game.world.width, Game.world.height).z = -1;
+            Crafty.e('Actor').img('images/background.jpg', "repeat")
+                .move(0, 0).size(Game.world.width, Game.world.height)
+                .z = -1;
         });
-        
+
         var p = Crafty.e('Player');
+        Crafty.viewport.clampEntities = false;
+        var scale = Math.min(Game.world.width / Game.view.width, Game.world.height / Game.view.height);
+        //Crafty.viewport.scale(scale);
+
         //Crafty.viewport.width = Game.view.width;
         //Crafty.viewport.height = Game.view.height;
-        Crafty.viewport.centerOn(p);
+        Crafty.viewport.follow(p);
+
+
     }
 }
 
