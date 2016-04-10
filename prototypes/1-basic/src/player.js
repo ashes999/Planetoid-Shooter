16e160489f7ebaf5376e5306c6d7cac19055470a
Ashiq A <ashes999@yahoo.com>
2016-04-09 20:52:01 -0400

    Player dies; game over

--- prototypes/1-basic/src/player.js
+++ prototypes/1-basic/src/player.js
@@ -7,5 +7,11 @@ Crafty.c('Player', {
         this.health -= damage;
         this.health = Math.max(this.health, 0);
         Crafty(Crafty('HealthBar')[0]).update(this.health);
+
+        if (this.health == 0) {
+            this.die();
+            Crafty(Crafty('Gun')[0]).die();
+            Crafty.e('Text2, FollowCamera').move(100, 100).fontSize(72).text('GAME OVER!');
+        }
     }
 });
